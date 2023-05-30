c5c1575527f8c5d20638cd87b7c38895186fbfb0
Marco <marcovirinni@gmail.com>
2023-05-30 14:21:57 -0300

    inicimos ejercicios de Phyton

---
+++ EJerciciosPhyton.py
@@ -0,0 +1,28 @@
+EJerciciosPhyton
+#   1.Realice un programa que solicite dos letras ingresadas por el usuario y
+#             verifique si son iguales o no, mostrando un mensaje.
+"""
+
+n=5
+n1=6
+
+if n == n1:
+    print("son iguales")
+else:
+    print("son distintos")    
+"""
+
+
+
+#    2. Hacer un programa que permita decidir si dos palabras son iguales o
+#                       diferentes. Mostrar mensaje por pantalla.
+
+
+palabra1 = input("Ingrese la primera palabra: ")
+palabra2 = input("Ingrese la segunda palabra: ")
+
+if palabra1 == palabra2:
+    print("Las palabras son iguales.")
+else:
+    print("Las palabras son diferentes.")
+
